63d0862acf84169d1e2ce94c2f1e6d7af57762a4
Abdurrahman Sogbesan <abdurrahman0803@gmail.com>
2020-12-14 21:54:35 +0100

    added header

---
+++ src/components/HeaderListItems.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex">
+    <HeaderListItem
+      :link="link"
+      v-for="(link, index) in HeaderListItems"
+      :key="index"
+    />
+  </div>
+</template>
+
+<script>
+import HeaderListItem from "./HeaderListItem.vue";
+import InboxIcon from "./InboxIcon.vue";
+
+export default {
+  data() {
+    return {
+      HeaderListItems: [
+        { text: "Help guide" },
+        { text: "Inbox", icon: InboxIcon }
+      ]
+    };
+  },
+  components: {
+    HeaderListItem
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
