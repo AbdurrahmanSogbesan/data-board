7f75a0693a698eaf1ed4b6a733b55b33d89f7caf
Habib Sogbesan <h.d.sogbesan@gmail.com>
2020-12-17 03:18:56 +0100

    style: fix header issues

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,40 +13,39 @@
 </template>
 
 <script>
-import TextBox from "./TextBox.vue";
-import SearchIcon from "./SearchIcon.vue";
-import HeaderListItems from "./HeaderListItems.vue";
-import Button from "./Button.vue";
+  import TextBox from "./TextBox.vue";
+  import SearchIcon from "./SearchIcon.vue";
+  import HeaderListItems from "./HeaderListItems.vue";
+  import Button from "./Button.vue";
 
-export default {
-  data() {
-    return {
-      SearchIcon
-    };
-  },
-  components: {
-    TextBox,
-    HeaderListItems,
-    Button
-  }
-};
+  export default {
+    data() {
+      return {
+        SearchIcon,
+      };
+    },
+    components: {
+      TextBox,
+      HeaderListItems,
+      Button,
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.header-section {
-  padding: 20px 22px;
-  justify-content: space-between;
-}
-.header-title {
-  font-size: 16px;
-  color: #a0aec0;
-  margin-right: 40px;
-}
-.image {
-  border-radius: 25px;
-}
-.header-list-items {
-  position: absolute;
-  right: 22px;
-}
+  .header-section {
+    padding: 20px 22px;
+    justify-content: space-between;
+    width: 82.5%;
+  }
+  .header-title {
+    font-size: 16px;
+    color: #a0aec0;
+    margin-right: 40px;
+  }
+  .image {
+    border-radius: 25px;
+  }
+  .header-list-items {
+  }
 </style>
